3fffb28dd7b54acd7a799bd8178667ccb35578c4
XiaoDouXd <1873316916@qq.com>
2026-02-18 18:51:36 +0800

    init 修正

--- tools/init_godot.ps1
+++ tools/init_godot.ps1
@@ -8,9 +8,18 @@ if ($exe) {
 
 New-Item -ItemType Directory -Force './gd_engine' | Out-Null
 $page = Invoke-WebRequest -UseBasicParsing https://godotengine.org/download/windows/
-$href = ($page.Links | Where-Object { $_.href -match 'Godot_v.*_stable_mono_win64\.zip' } | Select-Object -First 1).href
-if (-not $href) { throw 'Cannot find Godot mono win64 download link.' }
-if ($href -notmatch '^https?://') { $href = 'https://godotengine.org' + $href }
+$href = ($page.Links | Where-Object { $_.href -match 'Godot_v.*-stable_mono_win64\.zip' } | Select-Object -First 1).href
+
+if (-not $href) {
+  $match = [regex]::Match($page.Content, 'https?://[^"\s]*&slug=mono_win64.zip[^"\s]*&platform=windows.64')
+  if ($match.Success) {
+    $href = $match.Value
+  }
+}
+
+if (-not $href) { throw 'Cannot find Godot mono win64 download link. Please check https://godotengine.org/download/windows/ layout.' }
+if ($href -notmatch '^https?://') { $href = 'https://download.godotengine.org' + $href }
+
 
 $zip = Join-Path $env:TEMP 'godot_mono_win64.zip'
 Invoke-WebRequest -UseBasicParsing $href -OutFile $zip
